72021b2e838a4d6c2afa085dfa10ef61e24d0aa5
Henrique Camillo <henriquecamillo@usp.br>
2018-07-17 01:29:42 -0300

    GroundCheck added

--- CHeNOP/Assets/Scripts/Atom.cs
+++ CHeNOP/Assets/Scripts/Atom.cs
@@ -6,15 +6,21 @@ public class Atom : MonoBehaviour {
 	private Rigidbody2D rb;
 	[SerializeField] float speed;
 	[SerializeField] float jump;
+	private GameObject groundCheck;
+	private GroundCheck gndChckScript;
 
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		groundCheck = GameObject.FindGameObjectWithTag("GroundCheck");
+		gndChckScript = groundCheck.GetComponent<GroundCheck>();
 	}
 	
 	void Update () {
 			//Movement
 		rb.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), rb.velocity.y);
-		if(Input.GetKeyDown(KeyCode.W))
+		if((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && gndChckScript.grounded)
 			rb.AddForce(jump * Vector2.up, ForceMode2D.Impulse);
+
+		
 	}
 }
---
+++ CHeNOP/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+	public bool grounded;
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.gameObject.CompareTag("Ground"))
+			grounded = true;
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if(other.gameObject.CompareTag("Ground"))
+			grounded = false;
+	}
+}
