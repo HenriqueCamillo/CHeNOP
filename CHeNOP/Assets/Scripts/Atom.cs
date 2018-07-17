4bc064f0be041ca705774fc38a87e041e55bc7e6
Henrique Camillo <henriquecamillo@usp.br>
2018-07-17 19:02:35 -0300

    Atom change added

--- CHeNOP/Assets/Scripts/Atom.cs
+++ CHeNOP/Assets/Scripts/Atom.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Atom : MonoBehaviour {
+	private Animator atomAnimator;
 	private Rigidbody2D rb;
 	[SerializeField] float speed;
 	[SerializeField] float jump;
-	private GameObject groundCheck;
-	private GroundCheck gndChckScript;
-	private Vector2 respawn;
+	//private GameObject groundCheck;
+	[SerializeField] private GroundCheck gndChckScript;
+	private GameObject gameManager;
+	private GameManager gmMngrScrpit;
+	private bool facingRight = true;
 
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
-		groundCheck = GameObject.FindGameObjectWithTag("GroundCheck");
-		gndChckScript = groundCheck.GetComponent<GroundCheck>();
+		gameManager = GameObject.FindGameObjectWithTag("GameManager");
+		gmMngrScrpit = gameManager.GetComponent<GameManager>();		
+//		groundCheck = GameObject.FindGameObjectWithTag("GroundCheck");
+//		gndChckScript = groundCheck.GetComponent<GroundCheck>();
+		atomAnimator = GetComponent<Animator>();
 	}
 	
 	void Update () {
@@ -21,15 +27,21 @@ public class Atom : MonoBehaviour {
 		rb.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), rb.velocity.y);
 		if((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && gndChckScript.grounded)
 			rb.AddForce(jump * Vector2.up, ForceMode2D.Impulse);
+
+			//Animations
+		if(rb.velocity.x>0)
+			atomAnimator.SetBool("FacingRight", true);
+		else if(rb.velocity.x<0)
+			atomAnimator.SetBool("FacingRight", false);
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
 			//Collisions
 		if(other.gameObject.CompareTag("CheckPoint")){
-			respawn = other.transform.position;
+			gmMngrScrpit.respawn = other.transform.position;
 			Destroy(other.gameObject);
 			}
 		else if(other.gameObject.CompareTag("Antimatter"))
-			this.transform.position = respawn;
+			this.transform.position = gmMngrScrpit.respawn;
 	}
 }
--- CHeNOP/Assets/Scripts/GameManager.cs
+++ CHeNOP/Assets/Scripts/GameManager.cs
@@ -3,14 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+	public Vector2 respawn;
+	private var spawn;
+	[SerializeField] private GameObject[] atoms;
+	[SerializeField] private GameObject currentAtom;
+	[SerializeField] private int currentAtomNumber;
 
-	// Use this for initialization
+	enum atom {
+		Mercury = 0,
+		Helium = 1,
+		Lead = 2,
+		Phosphorus = 3,
+	};
+
 	void Start () {
-		
+		Instantiate(atoms[(int)atom.Mercury], this.transform);
+		currentAtomNumber = (int)atom.Mercury;
+		currentAtom = GameObject.FindGameObjectWithTag("Atom");
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		if(currentAtom == null)
+			currentAtom = GameObject.FindGameObjectWithTag("Atom");
+
+		if(Input.GetKeyDown(KeyCode.Alpha1) && currentAtomNumber!=(int)atom.Mercury){
+			currentAtomNumber = (int)atom.Mercury;
+			spawn = currentAtom.transform;
+			Destroy(currentAtom);
+			Instantiate(atoms[(int)atom.Mercury], spawn);
+			currentAtom = GameObject.FindGameObjectWithTag("Atom");
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha2) && currentAtomNumber!=(int)atom.Helium){
+			currentAtomNumber = (int)atom.Helium;
+			spawn = currentAtom.transform;
+			Destroy(currentAtom);
+			Instantiate(atoms[(int)atom.Helium], spawn);
+			currentAtom = GameObject.FindGameObjectWithTag("Atom");
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha3) && currentAtomNumber!=(int)atom.Lead){
+			currentAtomNumber = (int)atom.Lead;
+			spawn = currentAtom.transform;
+			Destroy(currentAtom);
+			Instantiate(atoms[(int)atom.Lead], spawn);
+			currentAtom = GameObject.FindGameObjectWithTag("Atom");
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha4) && currentAtomNumber!=(int)atom.Phosphorus){
+			currentAtomNumber = (int)atom.Phosphorus;
+			spawn = currentAtom.transform;
+			Destroy(currentAtom);
+			Instantiate(atoms[(int)atom.Phosphorus], spawn);
+			currentAtom = GameObject.FindGameObjectWithTag("Atom");
+		}
 	}
 }
