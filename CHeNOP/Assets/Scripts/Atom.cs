b3957184e9e7ff9d3645c36f63d91dedd70e7cae
Henrique Camillo <henriquecamillo@usp.br>
2018-07-17 00:39:32 -0300

    Moving Atom
    Atom movements added

---
+++ CHeNOP/Assets/Scripts/Atom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atom : MonoBehaviour {
+	private Rigidbody2D rb;
+	[SerializeField] float speed;
+	[SerializeField] float jump;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+			//Movement
+		rb.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), rb.velocity.y);
+		if(Input.GetKeyDown(KeyCode.W))
+			rb.AddForce(jump * Vector2.up, ForceMode2D.Impulse);
+	}
+}
